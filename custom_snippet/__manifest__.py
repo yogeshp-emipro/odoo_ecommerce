db023f6798fd1a8fe349102ed04281835d090782
Yogesh Pandey <yogeshp@emiprotechnologies.com>
2023-06-05 18:02:39 +0530

    [ADD] Adding new file to custom snippet.

---
+++ custom_snippet/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    'name': 'Custome Sinppet',
+    'author': 2.1,
+    'description': 'This module is for the demonstration purpose.',
+    'depends':['website'],
+    'data': ['views/custom_template.xml',
+             'views/custom_snippet.xml']
+}
